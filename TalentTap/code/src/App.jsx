d6a5a2cc661907be50563be151f960fab57e2ad3
FilledEther <gairolachaitanya@gmail.com>
2024-06-04 19:20:15 +0530

    JobCard implemented without routing

--- TalentTap/code/src/App.jsx
+++ TalentTap/code/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
 const App = () => {
 	return (
 		<>
 			<Navbar/>
 			<Banner />
+      <HomeCards/>
+      <JobListings/>
 		</>
 	);
 };
--- TalentTap/code/src/components/JobListings.jsx
+++ TalentTap/code/src/components/JobListings.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import jobsData from '../jobs.json';
+import JobCard from './JobCard';
 const JobListings = () => {
-  return (
-    <div>JobListings</div>
-  )
-}
+	const recentJobs=jobsData.jobs.slice(0,3)
+	return (
+		<section className="bg-blue-50 px-4 py-10">
+			<div className="container-xl lg:container m-auto">
+				<h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+					Recent Jobs
+				</h2>
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+					{recentJobs.map((job, id) => (
+						<JobCard
+							key={id}
+              type={job.type}
+							title={job.title}
+							salary={job.salary}
+							description={job.description}
+							location={job.location}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default JobListings
+export default JobListings;

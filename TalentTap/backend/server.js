591d6bfdb4c2a0beb87da22e769d781aaeaa4690
FilledEther <gairolachaitanya@gmail.com>
2024-07-05 03:30:07 +0530

    addJobs router and Mongodb connection made

--- TalentTap/backend/server.js
+++ TalentTap/backend/server.js
@@ -1,49 +1,37 @@
 const express = require('express');
-require("dotenv").config
-const URI=process.env.URL
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const Job = require('./models/Job');
 const PORT = 3000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//MongoDB Connection
+// MongoDB Connection
+const URI = process.env.URL;
 
+mongoose.connect(URI );
 
-app.post('/jobs', async (req, res) => {
-	try {
-		const {
-			title,
-			company,
-			date,
-			location,
-			salary,
-			description,
-			responsibilites,
-			requirements,
-		} = req.body;
-		const newJob = new Job({
-			title,
-			company,
-			dateOfPosting: date,
-			location,
-			salary,
-			description,
-			responsibilites,
-			requirements,
-		});
-		await newJob.save();
-		res.status(201).json({ message: 'Job posted successfully', job: newJob });
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ error: err.message });
-	}
+mongoose.connection.on('connected', () => {
+	console.log('Connected to MongoDB');
 });
+
+mongoose.connection.on('error', (err) => {
+	console.error(`MongoDB connection error: ${err}`);
+});
+
+const addJobs=require("./routes/AddJobs(Recruiter)")
+
+app.use('/addJob',addJobs)
+
 
 app.get('/', (req, res) => {
 	res.send('HI');
 });
+
 app.listen(PORT, () => {
-	console.log('HI');
+	console.log(`Server running on port ${PORT}`);
 });
